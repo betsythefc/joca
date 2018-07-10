4ff46c5bb7c3ae87a3bdd76287966b03a4d783e5
Bryce McNab <bmcnab@openmailbox.org>
2018-07-10 11:44:02 -0700

    Added quotes in if statements

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ current_version="v$(grep -E '^    version="[0-9]*\.[0-9]*\.[0-9]*",$' setup.py |
 check_for_tag=$(git tag | grep ${current_version})
 committed=$(git status | grep "nothing to commit")
 
-if [${committed} == ""];
+if ["${committed}" == ""];
 then
     echo "Commit your changes!"
     exit
 fi
 
-if [${check_for_tag} != ""];
+if ["${check_for_tag}" != ""];
 then
     last_commit=$(git log --name-status HEAD^..HEAD | head -1 | awk '{print $2}')
     git tag -a ${current_version} ${last_commit} -m "${current_version}"
