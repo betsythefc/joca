f224eec5b6fd39a1ee9fc8be81c14f8e0670165a
Bryce McNab <bmcnab@openmailbox.org>
2018-07-12 21:34:29 -0700

    Added licenses to all files

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# JOCA -- Jira On Call Assignee -- Change project lead based on an ical event.
+# Copyright (C) 2018 Bryce McNab
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 current_version="v$(grep -E '^    version="[0-9]*\.[0-9]*\.[0-9]*",$' setup.py | awk -F'"' '{print $2}')"
 check_for_tag=$(git tag | grep ${current_version})
 committed=$(git status | grep "nothing to commit")
