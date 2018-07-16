75a898afc63e3187251bef3bdcc4a87c08c4227b
Bryce McNab <bmcnab@openmailbox.org>
2018-07-15 21:53:59 -0700

    Corrected some pylint cruft

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except(IOError, ImportError):
 
 setuptools.setup(
     name="joca",
-    version="1.3.7",
+    version="1.3.8",
     author="Bryce McNab",
     author_email="brycemcnab@pm.me",
     description="Sync project lead with ical (for on call assignees)",
